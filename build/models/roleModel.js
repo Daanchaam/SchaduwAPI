569421e5975d5d6e7c02a9fedbbfc42deb9e7e33
Daanchaam <daanhendriks97@gmail.com>
2020-08-24 16:22:06 +0200

    Add build to gitignore

--- build/models/roleModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["ROLE.ADMIN"] = "admin";
-    Role["ROLE.REFEREE"] = "referee";
-    Role["ROLE.BASIC"] = "basic";
-})(Role = exports.Role || (exports.Role = {}));
